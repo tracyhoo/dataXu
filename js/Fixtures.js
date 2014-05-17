4e370127fcce6822ac4d7419380efcf64c42c62d
Tracy Hu <jiamin.hu.tracy@gmail.com>
2014-05-17 16:29:33 -0400

    initial commit for fixture data

---
+++ js/Fixtures.js
@@ -0,0 +1,74 @@
+App.User.FIXTURES = [
+ {
+ 	id: 1,
+
+	lastName: 'Abram',
+
+	firstName: 'Michael',
+
+	age:35,
+
+	email: 'mabram@example.com',
+
+	createdOn: '2014-05-16',
+
+	lastEdited: '2014-05-16',
+
+	active:true
+ },
+
+  {
+ 	id: 2,
+
+	lastName: 'Fenelly',
+
+	firstName: 'Kevin',
+
+	age:25,
+
+	email: 'kfenelly@example.com',
+
+	createdOn: '2014-05-16',
+
+	lastEdited: '2014-05-16',
+
+	active:true
+ },
+
+  {
+ 	id: 3,
+
+	lastName: 'Perry',
+
+	firstName: 'Jesse',
+
+	age:40,
+
+	email: 'jprerry@example.com',
+
+	createdOn: '2014-05-16',
+
+	lastEdited: '2014-05-16',
+
+	active:true
+ },
+
+  {
+ 	id: 4,
+
+	lastName: 'deGuerre',
+
+	firstName: 'Nom',
+
+	age:35,
+
+	email: 'ndeguerre@example.com',
+
+	createdOn: '2014-05-16',
+
+	lastEdited: '2014-05-16',
+
+	active:false
+ }
+
+];
